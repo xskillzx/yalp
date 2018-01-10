b1b302eb621c63ed206d0c840a1cb5529c7bee43
Peter Luu <peter.luu510@gmail.com>
2018-01-09 17:12:55 -0800

    (feat) Add server routes and server specs

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json('ok');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static asset...
+app.use(express.static(__dirname + '../client/dist'));
+
+// when user login, username, password
+app.get('/users/:username', (req, res) => {
+  res.status(200).json('ok');
+});
+
+// when user create
+app.post('/users', (req, res) => {
+  res.status(201).json('ok');
+});
+
+// when user search
+app.post('/businesses/search', (req, res) => {
+  res.status(201).json('ok');
+});
+
+// when user clicks on a business
+app.get('/businesses/:id', (req, res) => {
+  res.status(200).json('ok');
+});
+
+// when user clicks on his/her profile
+app.get('/profiles/:id', (req, res) => {
+  res.status(200).json('ok');
+});
+
+const server = app.listen(3000, () => {
+  const port = server.address().port;
+  console.log(`Listening at port ${port}`);
+});
+
+module.exports = server;
---
+++ server/test/serverSpec.js
@@ -0,0 +1,81 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const request = require('supertest');
+
+describe('loading express', function() {
+  let server;
+
+  beforeEach(function() {
+    server = require('../index.js');
+  });
+
+  afterEach(function() {
+    server.close();
+  });
+
+  it('should respond to GET /', function() {
+    return request(server)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to GET /users', function() {
+    return request(server)
+      .get('/users/username')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to POST /users', function() {
+    return request(server)
+      .post('/users')
+      .set('Accept', 'application/json')
+      .expect(201)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to POST /businesses/search', function() {
+    return request(server)
+      .post('/businesses/search')
+      .set('Accept', 'application/json')
+      .expect(201)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to GET /businesses/id', function() {
+    return request(server)
+      .get('/businesses/id')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to GET /profiles/id', function() {
+    return request(server)
+      .get('/profiles/id')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should 404 to GET /foo/bar', function(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});
