bd78873d5e8451c059a463f8fd7331fe8ddbb88e
kayfo23 <kayleigh.s.foley@gmail.com>
2018-01-11 11:56:25 -0800

    Separate db & server spec files and create test dir
    Refactor package.json & travis to account for altered spec files

---
+++ test/server-spec.js
@@ -0,0 +1,82 @@
+//SERVER SPECS
+const assert = require('assert');
+// const expect = require('chai').expect;
+const request = require('supertest');
+
+describe('loading express', function() {
+  let server;
+
+  beforeEach(function() {
+    server = require('../server/index.js');
+  });
+
+  afterEach(function() {
+    server.close();
+  });
+
+  it('should respond to POST /login', function() {
+    request(server)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'connorchen',
+        password: 'connorchen'
+      })
+      .expect(201)
+      .then(resp => {
+        expect(resp.body).to.eql([]);
+      });
+  });
+
+  it('should respond to POST /signup', function() {
+    request(server)
+      .post('/signup')
+      .set('Accept', 'application/json')
+      .send({
+        name: 'abc',
+        username: 'abc',
+        password: 'abc',
+        email: 'abc'
+      })
+      .expect(201)
+      .then(resp => {
+        expect(resp.body.affectedRows).to.equal(1);
+      });
+  });
+
+  it('should respond to POST /businesses/search', function() {
+    request(server)
+      .post('/businesses/search')
+      .set('Accept', 'application/json')
+      .expect(201)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to GET /businesses/id', function() {
+    request(server)
+      .get('/businesses/id')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should respond to GET /profiles/id', function() {
+    request(server)
+      .get('/profiles/id')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).to.equal('ok');
+      });
+  });
+
+  it('should 404 to GET /foo/bar', function(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});
