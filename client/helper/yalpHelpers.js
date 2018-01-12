ad759939b0ecfa56942a1e26872f43e24ffd1e32
Christopher Chen <chen1385@gmail.com>
2018-01-12 12:13:40 -0800

    refactored helper functions for GoogleAPI

---
+++ client/helper/yalpHelpers.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const config = require('../../config.js');
+
+const googleAPI = `https://maps.googleapis.com/maps/api/place/`
+const location = `location=37.7749,-122.4194`
+
+const getBusinessInfo = (businessID, cb) => {
+    axios.get(`${googleAPI}details/json?placeid=${businessID}&key=${config.YELP_API_KEY}AIzaSyAydLGe6HgJ7uE8mDbqj8v-ccMMHnV9XEw`)
+        .then(response => cb(response))
+        .catch(error => console.log('error:', error))
+}
+
+const searchBusinesses = (query, cb) => {
+    axios.get(`${googleAPI}textsearch/json?query=${query}&${location}&key=${config.YELP_API_KEY}`)
+        .then(response => cb(response))
+        .catch(error => console.log('error:', error))
+}
+
+module.exports = {
+    getBusinessInfo,
+    searchBusinesses
+}
