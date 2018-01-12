00a5d5a406865d5e033d8e99fad79b1d22414ad3
kayfo23 <kayleigh.s.foley@gmail.com>
2018-01-12 11:55:40 -0800

    Add review component design & button functionality

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -2,12 +2,61 @@ import React from 'react';
 
 class AddReview extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      rating: 1,
+      text: ''
+    }
+    this.updateRating = this.updateRating.bind(this);
+    this.updateReviewText = this.updateReviewText.bind(this);
+    this.submitReview = this.submitReview.bind(this);
+  }
+
+  updateRating(val) {
+    this.setState({ rating: val })
+  }
+  updateReviewText(e) {
+    this.setState({ text: e.target.value })
+  }
+  submitReview() {
+    //add review to db (user, rating, text)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Leave a Review</div>
+        <button className="rate-btn" onClick={() => this.updateRating(5)}>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+        </button>
+        <button className="rate-btn" onClick={() => this.updateRating(4)}>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+        </button>
+        <button className="rate-btn" onClick={() => this.updateRating(3)}>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+        </button>
+        <button className="rate-btn" onClick={() => this.updateRating(2)}>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+        </button>
+        <button className="rate-btn" onClick={() => this.updateRating(1)}>
+          <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
+        </button>
+        <textarea id="review-text" type="text" placeholder="What'd you think?" onChange={this.updateReviewText}></textarea>
+        <button className="review-submit" onClick={this.submitReview}>Submit</button>
+      </div>
+    )
   }
 }
 
-render() {
-  
-}
 
 export default AddReview;
