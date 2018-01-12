1c6bb010f69ba0fe114bf72f78ff9ae473f3d255
Christopher Chen <chen1385@gmail.com>
2018-01-12 14:53:03 -0800

    fixed bug in helper functions & revised config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    YELP_API_KEY: 'xlsMIIoQbVZNbkQgoTeOhj_rhTT2J_EOUvcN6ZPfOnWOT7dZ6SAyTFI0NW8D8mIpN0npcZg4SXxvq4E9td_FUh-GEQ_1CFFsnro587xweEgCZYfkWgZy7PaV_wxZWnYx',
-    CLIENT_ID: 'SJ_I8MziA0n2UlqFGe_cfA'
+    GOOGLE_API_KEY: AIzaSyAydLGe6HgJ7uE8mDbqj8v-ccMMHnV9XEw
 }
