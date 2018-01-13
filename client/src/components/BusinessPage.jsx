d80583a85d219cb498982a03d217c0800155c454
connorc1231 <connorc1231@gmail.com>
2018-01-13 11:55:54 -0800

    Working build update

--- client/src/components/BusinessPage.jsx
+++ client/src/components/BusinessPage.jsx
@@ -4,10 +4,27 @@ import BusinessInfo from './BusinessInfo.jsx';
 import BusinessMap from './BusinessMap.jsx'
 import PhotoFeed from './PhotoFeed.jsx';
 import Reviews from './Reviews.jsx';
+import config from '../../../config.js'
 
 class BusinessPage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      mapData: {}
+    }
+  }
+
+  componendDidMount() {
+    getMap(this.props.business.geometry.location);
+  }
+
+  getMap(location, cb) {
+  axios.get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+    &markers=color:blue%7Clabel:S%7C${location.lat}, ${location.lng}
+    &key=${config.GOOGLE_API_KEY}`
+  )
+    .then(response => console.log(response) )
+    .catch(error => console.log('error:', error))
   }
 
   render() {
