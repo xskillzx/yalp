430a70e50f78ea7a5181a6564642386eced5df2f
connorc1231 <connorc1231@gmail.com>
2018-01-12 17:15:36 -0800

    Updating Master repo

--- client/helper/yelpHelpers.js
+++ client/helper/yelpHelpers.js
@@ -15,26 +15,24 @@ const searchBusinesses = (query, cb) => {
     .catch(error => console.log('error:', error))
 }
 
-
 const getPhoto = (ref, cb) => {
     axios.get(`${googleAPI}photo?maxwidth=175&photoreference=${ref}&key=${GOOGLE_API_KEY}`)
     .then(response = cb(response))
     .catch(error => console.log('error:', error))
 }
 
-const getMap = () => {
-    axios.get(`${googleAPI}details/json?reference=${businessRef}&key=${GOOGLE_API_KEY}`)
-      .then(response => cb(response))
-      .catch(error => console.log('error:', error))
-    axios.get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-      &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
-      &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-      &key=${GOOGLE_API_KEY}`
-    })
-  }
+const getMap = (location, cb) => {
+  axios.get(`${googleAPI}details/json?reference=${businessRef}&key=${GOOGLE_API_KEY}`)
+  axios.get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+    &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+    &key=${GOOGLE_API_KEY}`
+  )
+    .then(response => cb(response))
+    .catch(error => console.log('error:', error))
+}
 
 module.exports = {
-    getBusinessInfo,
-    searchBusinesses,
-    getPhoto
-
+  getBusinessInfo,
+  searchBusinesses,
+  getPhoto
+}
--- client/src/components/BusinessPage.jsx
+++ client/src/components/BusinessPage.jsx
@@ -10,19 +10,6 @@ class BusinessPage extends React.Component {
     super(props)
   }
 
-  getMap() {
-    axios.get(`${googleAPI}details/json?reference=${businessRef}&key=${GOOGLE_API_KEY}`)
-      .then(response => cb(response))
-      .catch(error => console.log('error:', error))
-    axios.get(`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-      &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
-      &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-      &key=YOUR_API_KEY`
-
-
-    })
-  }
-
   render() {
     return (
       <div className="businessPage">
