e91d01288b88f7f369b5a14a565455caa77cadbd
kayfo23 <kayleigh.s.foley@gmail.com>
2018-01-13 20:29:04 -0800

    Stub out friends checkins component

--- client/src/components/FriendActivity.jsx
+++ client/src/components/FriendActivity.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FriendActivityEntry from './FriendActivityEntry.jsx';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
+import axios from 'axios';
 
 //business obj, username, userId
 
@@ -58,11 +59,23 @@ class FriendActivity extends React.Component {
   }
 
   getFriendCheckins() {
-    // use userId and businessId to get all checkins
-    // query for username & createdAt for that business
-    // update friendCheckins state (friend username, createdAt)
 
-    // update ul below 
+    // query for username & createdAt for that business
+
+    axios.get('/server/business/checkins', {
+      params: {
+        businessId: this.props.business.id,
+        userId: this.props.userId
+      }
+    })
+      .then(response => {
+        this.setState({
+          friendCheckins: response
+        })
+      })
+      .catch (err => {
+        if (err) { console.log(err) }
+      })
   }
 
   render() {
@@ -70,7 +83,11 @@ class FriendActivity extends React.Component {
       <div>
         <div className="activity-header">Your Friends' Recent Activity...</div>
         <div className="check-in-header">Check-ins:</div>
-        <ul>{this.getFriendActivityEntries()}</ul>
+        <ul>
+          {this.state.friendCheckins.map((checkin, index) => {
+            <FriendActivityEntry checkin={checkin} key={index} />
+          })}
+        </ul>
       </div>
     );
   }
