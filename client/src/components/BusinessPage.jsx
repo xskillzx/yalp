929098a2ae9be79057dab810238a5cf55540f784
kayfo23 <kayleigh.s.foley@gmail.com>
2018-01-15 10:42:54 -0800

    Styling

--- client/src/components/BusinessPage.jsx
+++ client/src/components/BusinessPage.jsx
@@ -23,15 +23,23 @@ class BusinessPage extends React.Component {
         <Search getBusinesses={this.props.getBusinesses}/>
         <BusinessInfo business={this.props.business}/>
         {/* {this.props.checkedIn ? <button className="checkIn">Checked In!</button> : <button className="checkIn" onClick={() => this.props.checkIn(this.props.business)}>Check In</button>} */}
-        {this.props.checkedIn ? <div onClick={e => {this.props.checkIn(this.props.business)}} className="checkIn">Already Checked In!</div> :
-          <div onClick={e => {this.props.checkIn(this.props.business)}} className="checkIn">Check In</div> }
-        <div>
-          {
-            this.props.getFavoriteInfo(this.props.business.id) ?
-            <button className="favoriteIn disabled">Favorited</button>
-            : <button className="favoriteIn" onClick={e => {this.props.favoriteIn(this.props.business)}}>Favorite</button>
-          }
+        <div className="business-page-btns">
+          <div>
+            {
+              this.props.checkedIn ?
+              <div onClick={e => {this.props.checkIn(this.props.business)}} className="checkIn">Already Checked In!</div> :
+              <div onClick={e => {this.props.checkIn(this.props.business)}} className="checkIn">Check In</div>
+            }
+          </div>
+          <div>
+            {
+              this.props.getFavoriteInfo(this.props.business.id) ?
+              <button className="favoriteIn disabled">Favorited</button> :
+              <button className="favoriteIn" onClick={e => {this.props.favoriteIn(this.props.business)}}>Favorite</button>
+            }
+          </div>
         </div>
+
         <div className="addReview">
           <AddReview business={this.props.business} username={this.props.username} userId={this.props.userId} />
         </div>
