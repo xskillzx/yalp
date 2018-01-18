575dc4fd6a2caa392d9a083c5675a1c28051b61c
justin <justindulark@gmail.com>
2018-01-18 08:25:04 -0800

    Change radio buttons to handle rating

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -17,23 +17,23 @@ class AddReview extends React.Component {
   }
 
   updateRating(val, e) {
-    console.log(this.lastSelectedRating)
-    if (this.lastSelectedRating) {
-      console.log('found last rating element', this.lastSelectedRating)
-      this.lastSelectedRating.style.background = 'red';
-    }
-    this.lastSelectedRating = e.currentTarget;
-     if (e.currentTarget.style.background === 'yellow') {
-      e.currentTarget.style.background = 'red';
-      for (let i = 0; i < e.currentTarget.children.length; i++) {
-        e.currentTarget.children[i].background = 'red';
-      }
-     } else {
-      e.currentTarget.style.background = 'yellow';
-      for (let j = 0; j < e.currentTarget.children.length; j++) {
-        e.currentTarget.children[j].background = 'yellow';
-      }
-     }
+    // console.log(this.lastSelectedRating)
+    // if (this.lastSelectedRating) {
+    //   console.log('found last rating element', this.lastSelectedRating)
+    //   this.lastSelectedRating.style.background = 'red';
+    // }
+    // this.lastSelectedRating = e.currentTarget;
+    //  if (e.currentTarget.style.background === 'yellow') {
+    //   e.currentTarget.style.background = 'red';
+    //   for (let i = 0; i < e.currentTarget.children.length; i++) {
+    //     e.currentTarget.children[i].background = 'red';
+    //   }
+    //  } else {
+    //   e.currentTarget.style.background = 'yellow';
+    //   for (let j = 0; j < e.currentTarget.children.length; j++) {
+    //     e.currentTarget.children[j].background = 'yellow';
+    //   }
+    //  }
     this.setState({ rating: val })
   }
   updateReviewText(e) {
@@ -69,7 +69,7 @@ class AddReview extends React.Component {
       return (
         <div>
           <div>Leave a Review</div>
-          <button className="rate-btn" onClick={e => this.updateRating(5, e)}>
+          {/* <button className="rate-btn" onClick={e => this.updateRating(5, e)}>
             <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
             <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
             <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
@@ -93,18 +93,18 @@ class AddReview extends React.Component {
           </button>
           <button className="rate-btn" onClick={e => this.updateRating(1, e)}>
             <img className="ratingLogo" src='https://image.ibb.co/bzkXSR/imageedit_12_7791151374.png' width='20px'/>
-          </button>
+          </button> */}
           <fieldset className="rating">
-            <input type="radio" id="star5" name="rating" value="5" /><label className = "full" htmlFor="star5" title="Awesome - 5 stars"></label>
-            <input type="radio" id="star4half" name="rating" value="4 and a half" /><label className="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
-            <input type="radio" id="star4" name="rating" value="4" /><label className = "full" htmlFor="star4" title="Pretty good - 4 stars"></label>
-            <input type="radio" id="star3half" name="rating" value="3 and a half" /><label className="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
-            <input type="radio" id="star3" name="rating" value="3" /><label className = "full" htmlFor="star3" title="Meh - 3 stars"></label>
-            <input type="radio" id="star2half" name="rating" value="2 and a half" /><label className="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
-            <input type="radio" id="star2" name="rating" value="2" /><label className = "full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
-            <input type="radio" id="star1half" name="rating" value="1 and a half" /><label className="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
-            <input type="radio" id="star1" name="rating" value="1" /><label className = "full" htmlFor="star1" title="Sucks big time - 1 star"></label>
-            <input type="radio" id="starhalf" name="rating" value="half" /><label className="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
+            <input type="radio" id="star5" name="rating" value="5"  onClick={e => this.updateRating(5, e)}/><label className = "full" htmlFor="star5" title="Awesome - 5 stars"></label>
+            <input type="radio" id="star4half" name="rating" value="4 and a half"  onClick={e => this.updateRating(4.5, e)}/><label className="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
+            <input type="radio" id="star4" name="rating" value="4"  onClick={e => this.updateRating(4, e)}/><label className = "full" htmlFor="star4" title="Pretty good - 4 stars"></label>
+            <input type="radio" id="star3half" name="rating" value="3 and a half"  onClick={e => this.updateRating(3.5, e)}/><label className="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
+            <input type="radio" id="star3" name="rating" value="3"  onClick={e => this.updateRating(3, e)}/><label className = "full" htmlFor="star3" title="Meh - 3 stars"></label>
+            <input type="radio" id="star2half" name="rating" value="2 and a half"  onClick={e => this.updateRating(2.5, e)}/><label className="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
+            <input type="radio" id="star2" name="rating" value="2"  onClick={e => this.updateRating(2, e)}/><label className = "full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
+            <input type="radio" id="star1half" name="rating" value="1 and a half"  onClick={e => this.updateRating(1.5, e)}/><label className="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
+            <input type="radio" id="star1" name="rating" value="1"  onClick={e => this.updateRating(1, e)}/><label className = "full" htmlFor="star1" title="Sucks big time - 1 star"></label>
+            <input type="radio" id="starhalf" name="rating" value="half"  onClick={e => this.updateRating(0.5, e)}/><label className="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
           </fieldset>
           <textarea className="new-review-text" type="text" placeholder="What'd you think?" onChange={this.updateReviewText}></textarea>
           <button className="review-submit" onClick={this.submitReview}>Submit</button>
