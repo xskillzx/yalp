f50a7b554daa350caa59f0b034ae28d266f58701
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-17 18:52:49 -0800

    Add user granted geolocation which will be visually presented in a human friendly way in the input field and use the gained geocode for their search

--- client/src/components/GoogleSuggest.jsx
+++ client/src/components/GoogleSuggest.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import GoogleMapLoader from "react-google-maps-loader";
 import GooglePlacesSuggest from "react-google-places-suggest";
  
-const MY_API_KEY = "AIzaSyBdR4CN_ZcKvi-gmm4cwkB_4RH-jnJNBCc";
+const MY_API_KEY = "AIzaSyAMML28IbvEgj5ssCZ7SLoSmqMMS6_Kqbw";
  
 export default class GoogleSuggest extends React.Component {
   constructor(props) {
@@ -11,6 +11,27 @@ export default class GoogleSuggest extends React.Component {
 	    search: "",
 	    value: "",
 	  }
+  }
+
+  userGrantedLocation() {
+  	console.log('inside user granted location');
+    let geo_success = position => {
+    	let latLng = {lat: position.coords.latitude, lng: position.coords.longitude}
+    	this.geocodeLatLng(latLng, (results) => {
+    		this.setState({ value: results[0].formatted_address })
+    		this.props.locChange(`(${position.coords.latitude}, ${position.coords.longitude})`)
+    	});
+    }
+
+    let geo_error = position => {
+      alert("Sorry, no position available.");
+    }
+
+    var geo_options = {
+      enableHighAccuracy: true
+    };
+
+    navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
   }
  
   handleInputChange(e) {
@@ -35,9 +56,33 @@ export default class GoogleSuggest extends React.Component {
       }
     });
   }
+
+  geocodeLatLng(latlng, cb) {
+  	var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'location': latlng}, (results, status) => {
+      if (status === 'OK') {
+      	// this.props.locChange(results[0].geometry.location.toString());
+      	cb(results);
+      	return results[0].geometry.location.toString();
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+        return undefined;
+      }
+    });
+  }
  
   render() {
+    let getLocation;
     const {search, value} = this.state
+
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      getLocation = <button onClick={this.userGrantedLocation.bind(this)}>Get your location</button>
+    } else {
+      /* geolocation IS NOT available */
+      getLocation = undefined;
+    }
+
     return (
       <GoogleMapLoader
         params={{
@@ -64,6 +109,8 @@ export default class GoogleSuggest extends React.Component {
                 </div>
               )}
             >
+				      <h3>Location</h3>
+				      {getLocation}
               <input
                 type="text"
                 value={value}
