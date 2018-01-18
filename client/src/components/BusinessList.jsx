41cd95961a4326d89c855e61fe677caace3e1ac2
Felipe Catania <felicatania@gmail.com>
2018-01-17 18:33:10 -0800

    Fix GoogleMap integration

--- client/src/components/BusinessList.jsx
+++ client/src/components/BusinessList.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 class BusinessList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {searchResults: []};
+    this.state = {searchResults: [], initLocation: {}};
   }
   componentWillMount() {
     document.body.style.background = "url('wood.jpg')";
@@ -16,7 +16,7 @@ class BusinessList extends React.Component {
     document.body.style.backgroundRepeat = "repeat-y";
   }
 
-  getBusinesses(search, loc = `37.7749,-122.4194`) {
+  getBusinesses(search, loc = `(37.7749,-122.4194)`) {
     let url = `/server/search/${search}/${loc}`;
     axios.get(url)
     .then(resp => {
@@ -29,6 +29,12 @@ class BusinessList extends React.Component {
 
   componentDidMount() {
     let obj = (this.props.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+    if (obj.location) {
+      let locationStr = obj.location.slice(1, -1).replace('%20', '');
+      let locationArr = locationStr.split(',');
+      let locationObj = {lat: locationArr[0], lng: locationArr[1]};
+      this.setState({initLocation: locationObj});
+    }
     this.getBusinesses(obj.search, obj.location);
   }
 
@@ -49,7 +55,7 @@ class BusinessList extends React.Component {
           {this.getBusinessEntries()}
         </div>
         <div id="map-container">
-          <MapContainer businesses={this.props.businesses.data}/>
+          {this.state.searchResults.length > 0 && <MapContainer initLocation={this.state.initLocation.lat ? this.state.initLocation : {lat: '37.7749', lng: '-122.4194'}} businesses={this.state.searchResults}/>}
         </div>
       </div>
     )
