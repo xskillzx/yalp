ae734ef03c8737dfc06ab90ba9b19cbf513100ae
Franco Catania <catania.fco@gmail.com>
2018-01-19 12:06:56 -0800

    search for results when pressing enter

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -13,11 +13,17 @@ class Search extends React.Component {
     this.setState({ latLin : loc });
   }
 
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.goToListings(this.refs.searchTerm.value, this.state.latLin);
+    }
+  }
+
   render() {
     return (
       <div>
         <Suggest locChange={this.latLinChange.bind(this)}/>
-        <input className="searchBar" ref="searchTerm" type="text" size="30" placeholder="What's nearby?" />
+        <input className="searchBar" ref="searchTerm" onKeyPress={this.handleKeyPress.bind(this)} type="text" size="30" placeholder="What's nearby?" />
         <button type="Search" onClick={e => this.props.goToListings(this.refs.searchTerm.value, this.state.latLin)}>
           Search!
         </button>
